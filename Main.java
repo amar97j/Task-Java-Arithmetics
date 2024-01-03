22c9c0fd21e508d162c4c8fb8c5c90b5e10b5194
Amar J M H M H  Alfailakawi <83320@kfh.com>
2024-01-03 08:47:10 +0300

    hello

---
+++ Main.java
@@ -0,0 +1,21 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class Main {
+  public static void main(String[] args) {
+
+    double amountKWD = 86;
+    double amountUSD = 100;
+
+    System.out.println(amountKWD + "  " + "KWD = " + amountKWD * 3.25 + " " + "USD");
+
+    System.out.println(amountUSD + "  " + "USD = " + amountUSD / 3.25 + " " + "KWD");
+
+  }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
+}
